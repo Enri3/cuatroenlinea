8a154dad3e2578f4511b8ee7f06159b15c34ba21
Enri3 <ereschini06@gmail.com>
2022-06-28 18:33:50 -0300

    no fue el ultimo commit :(

--- tests/Feature/myFirstTest.php
+++ tests/Feature/myFirstTest.php
@@ -14,31 +14,45 @@ class myFirstTest extends TestCase
      * @return void
      */
 
-    public function test_example()
+    public function test_the_web_it_is_online()
     {
-        $response = $this->get('/');
+        $page = $this->get('https://cuatroenlinea.ddev.site/jugar/1');
 
-        $response->assertStatus(200);
+        $page->assertStatus(200);
     }
 
     public function test_red(){
 
-        $html = file_get_contents('/jugar/31313');
+        $html = file_get_contents('https://cuatroenlinea.ddev.site/jugar/31313');
 
         $this->assertTrue(substr_count($html,'bg-red-500') == 3);
     }
 
     public function test_blue(){
 
-        $html = file_get_contents('/jugar/1212');
+        $html = file_get_contents('https://cuatroenlinea.ddev.site/jugar/1212');
 
         $this->assertTrue(substr_count($html,'bg-sky-500') == 2);
     }
 
     public function test_table(){
 
-        $html = file_get_contents('/jugar/1');
+        $html = file_get_contents('https://cuatroenlinea.ddev.site/jugar/1');
 
         $this->assertTrue(substr_count($html,'bg-gray-200') == 41);
+    }
+
+    public function test_next_movement(){
+
+        $html = file_get_contents('https://cuatroenlinea.ddev.site/jugar/1');
+
+        $this->assertTrue(substr_count($html,'hover:animate-spin') == 7);
+    }
+
+    public function test_i_am_not_ready_for_this(){
+
+        $page = $this->get('https://cuatroenlinea.ddev.site/jugar/1111112222223333334444445555556666667777771');
+
+        $page->assertStatus(200);
     }
 }
