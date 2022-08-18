68e832c9ba7b276b78fd435eba150780ea4e64bd
Enrico Reschini <ereschini@wiener-lab.net>
2022-08-18 09:47:02 -0300

    __construct y ponerFicha creadas.

--- app/Tablero.php
+++ app/Tablero.php
@@ -1,3 +1,46 @@
 <?php
 namespace App;
+
+use App\Piece;
+
+interface interfaseTablero{
+
+    public function ponerFicha(int $col, array $colorFicha) //pone una ficha en el tablero.
+}
+
+class Tablero implements interfaseTablero{
+
+    protected int $dim_x;
+    protected int $dim_y;
+
+    protected array $tablero;
+}
+
+public function __construct(int $dim_x = 7, int $dim_y = 6){
+
+    if(dim_x < 4 and dim_y < 4){
+        throw new Exception('Las dimensiones deben tener al menos 4 de ancho y largo para poder jugar.');
+    }
+
+    $this->dim_x = $dim_x
+    $this->dim_y = $dim_y
+}
+
+public function ponerFicha(int $col, array $colorFicha){
+
+    if($col < 0 || $col > $this->dim_x){
+        throw new Exception('No se pueden poner fichas por fuera del tablero.');
+    }
+
+    if(strlen($this->tablero[$col]) >= $this->dim_y){
+        throw new Exception('Esta columna se encuentra llena.');
+    }
+
+    if($colorFicha != 'rojo' && $colorFicha != 'azul'){
+        throw new Exception('Solo se aceptan fichas rojas y azules.');
+    }
+
+    $this->tablero[$col]->append($colorFicha);
+}
+
 ?>
