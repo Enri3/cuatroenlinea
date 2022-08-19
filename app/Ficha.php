ad98958b9b388e09e9a958558d04aad7755a1d11
Enrico Reschini <ereschini@wiener-lab.net>
2022-08-19 09:59:08 -0300

    clase ficha terminada pero no probada

--- app/Ficha.php
+++ app/Ficha.php
@@ -1,3 +1,16 @@
 <?php
 namespace App;
+
+class Ficha{
+
+	public function __construct($color){
+		$this->color = $color;
+	}
+
+	protected $color;
+
+	public function mostrarColor(){
+		return $this->color;
+	}
+}
 ?>
